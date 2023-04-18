63e6f572b05e5727a295f80883c5ebab89931918
Chechox89 <scali.89@hotmail.com>
2023-04-17 20:36:14 -0600

    Agregue files a node API repo

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express")
+const app=express()
+const port=8898
+
+const array=[
+    
+{
+    name:"john wick",
+    age:"90"
+},
+
+{
+
+  name:"Chana Juana",
+  age:"20"  
+}
+
+
+]
+
+
+
+
+app.get("/", (req,res)=>{
+    res.send("Hola clase!")
+})
+
+app.get("/getNames", (req,res)=>{
+    res.send(array)
+})
+
+app.get("/about",(req,res) =>{
+    res.send("What do you want to know")
+})
+
+
